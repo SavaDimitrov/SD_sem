cd9851c0a759c3f11d4e5d94bdea36d9ff4c1803
SavaDimitrov <savad.9310@gmail.com>
2018-01-09 11:56:30 +0200

    Add files via upload

---
+++ hashing.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+//#include <dirent.h>
+
+using namespace std; 
+
+
+//za 5-to domashno
+//
+//void listFile() {
+//	DIR *dir;
+//	dirent *entry;
+//	//mkdir(); //pravi nova direktoriq v tekushtata
+//
+//	if (dir = opendir("./")){ //file/folder location, ./ -> tekushta directoriq (this-a)
+//								//.. -> predishna directoriq
+//		while (entry = readdir(directory)){
+//			cout << entry->d_name << "\n";
+//			/*if (entry->d_type == DT_REG){
+//				cout << "FILE" << "\n";*/
+//			}
+//		closedir(directoy);
+//		}
+//	}
+//}
+//
+
+
+int hashing(string const& str) {
+	int h = 0;
+	//...
+	return h;
+}
+
+int main() {
+
+	
+
+
+	system("pause");
+	return 0;
+}
